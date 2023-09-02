2815e2319d3414e16f15cb6bfeabf938ed19181c
GabrielInacio03 <inaciogs03@gmail.com>
2023-09-02 12:37:44 -0300

    criando a classe posicaoXadrez

---
+++ xadrez/xz/PosicaoXadrez.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xadrez.tabuleiro;
+
+namespace xadrez.xz
+{
+    public class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + coluna + "" + linha ;
+        }
+    }
+}
